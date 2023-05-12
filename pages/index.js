89a4bb1c7e30ffc56f8786c256c7e2de195232e1
fawadahmedraza <fawadahmedr@gmail.com>
2023-05-13 01:47:19 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Configuration, OpenAIApi } from "openai";
+import { useEffect, useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const configuration = new Configuration({
+  // organization: "org-GQvyGcX8tf3sOxQfFDsne0fY",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [sign, setSign] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    // Use OpenAI's GPT-3 API to generate a response to the user's question
+    const prompt = `The user ${name} (${dob}) asks: for only  astrological sign name also reply this question " ${question}"\n\nResponse:`;
+    const res = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+    });
+
+    if (res.data) {
+      setResponse(res.data.choices[0].text.trim());
+      setLoading(false);
+    } else {
+      console.log(res);
+      setLoading(false);
+    }
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -11,41 +45,76 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">Astro Ai</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Get started by entering date of birth and name
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="w-100">
+          <form onSubmit={handleSubmit} className="w-100">
+            <div className="row w-100">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>
+                    Name:
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>
+                    Date of birth:
+                    <input
+                      className="form-control"
+                      type="date"
+                      value={dob}
+                      onChange={(e) => setDob(e.target.value)}
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <br />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="container">
+              <div className="row w-100">
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label>
+                      Question:
+                      <textarea
+                        className="form-control"
+                        cols={50}
+                        value={question}
+                        onChange={(e) => setQuestion(e.target.value)}
+                      />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <button type="submit" className="mt-2 btn btn-primary">
+              Submit
+            </button>
+          </form>
+          {loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <div>
+              <h2>Response:</h2>
+              <p>{response}</p>
+            </div>
+          )}
         </div>
       </main>
 
@@ -55,7 +124,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -95,6 +164,43 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
+
+        .lds-circle {
+          display: inline-block;
+          transform: translateZ(1px);
+        }
+        .lds-circle > div {
+          display: inline-block;
+          width: 64px;
+          height: 64px;
+          margin: 8px;
+          border-radius: 50%;
+          background: #fff;
+          animation: lds-circle 2.4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+        }
+        @keyframes lds-circle {
+          0%,
+          100% {
+            animation-timing-function: cubic-bezier(0.5, 0, 1, 0.5);
+          }
+          0% {
+            transform: rotateY(0deg);
+          }
+          50% {
+            transform: rotateY(1800deg);
+            animation-timing-function: cubic-bezier(0, 0.5, 0.5, 1);
+          }
+          100% {
+            transform: rotateY(3600deg);
+          }
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
       `}</style>
 
       <style jsx global>{`
@@ -111,5 +217,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
