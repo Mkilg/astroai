2819d9a5ae65a17b8bcd410e5090caecfe954a67
Fawad Ahmed Raza <68689723+FawadAhmedRaza@users.noreply.github.com>
2023-05-17 13:12:29 +0500

    change in model for temp testing

--- pages/api/openaiapi.js
+++ pages/api/openaiapi.js
@@ -10,7 +10,8 @@ const openai = async (req, res) => {
     const { prompt } = JSON.parse(req.body);
 
     const openaiRes = await openaiApi.createCompletion({
-      model: "gpt-3.5-turbo",
+      // model: "gpt-3.5-turbo",
+      model:"text-davinci-003",
       prompt,
     });
 
